467796138c726c775493be45633a654a7799231d
soumojitjati51-creator <soumojitjati51@gmail.com>
2026-07-12 09:00:49 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,7 @@
+export var a=20
+// const user=[10,20,30,40]
+// export default a
+
+const user=[10,20,30,40]
+export default user
+
---
+++ script.js
@@ -0,0 +1,25 @@
+//we can create an element in html through js
+// var h1=document.createElement('h1')
+// h1.innerHTML='Hello from JS'
+// document.body.appendChild(h1)
+// console.log(h1);
+
+
+//Library  used to implement a particular feature
+  //-GSAP
+  //-Lenis
+  //-ReactJS
+//Framework   handles all thing , gets thing in-built
+  //-NextJS
+  //-Angular
+// import a from './app.js'
+//   console.log(a);
+
+import arr from './app.js'//default export ....names can be different in two files
+import {a} from './app.js'// named export ...with {} exact name should given
+
+console.log(arr);
+console.log(a);
+
+
+

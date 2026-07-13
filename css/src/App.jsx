dbe95ef2c67956e09f004cc66ee63d9808544245
soumojitjati51-creator <soumojitjati51@gmail.com>
2026-07-13 17:17:57 +0530

    Learned module css structure ,to create separate folder for each components and put its jsx and css file together so that css stylings does not get clumsy and get separated and organized and for a particular class of style applies only to a particular component which can't be maintained in global index.css

---
+++ css/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from './components/Button/Button.jsx'
+import Header from './components/Header/Header.jsx'
+
+const App = () => {
+  return (
+    <div>
+      <Header/>
+      <Button/>
+    </div>
+  )
+}
+
+export default App
